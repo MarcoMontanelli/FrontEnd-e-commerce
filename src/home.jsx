8bc798faebec5fb037aa5abe68a65b69752ac281
MarcoMontanelli <101709469+MarcoMontanelli@users.noreply.github.com>
2024-05-11 08:28:01 +0200

    Initial commit

---
+++ src/home.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div className="text-xl font-medium text-black">Welcome to Vite + React + Tailwind!</div>
+        <p className="text-gray-500">You're ready to build something amazing.</p>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App
